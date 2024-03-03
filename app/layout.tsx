a3ed7f237facd6bebceafc9d1f4b8a85dc81d58e
muhammedshareef200 <muhammedshareef200@gmail.com>
2024-03-03 20:23:54 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-poppins',
+})
+
+export const metadata: Metadata = {
+  title: 'EventEase',
+  description: 'EventEase: Simplify event planning with user-friendly features, intuitive management, and secure transactions for effortless gatherings ',
+  icons: {
+    icon: '/assets/images/eventease.png'
+  }
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
