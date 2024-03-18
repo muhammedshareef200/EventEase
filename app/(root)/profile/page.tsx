f8f6a2307b74cf190c30f3dd5bdb386572197d5f
muhammed shareef <muhammedshareef200@gmail.com>
2024-03-18 20:27:16 +0530

    finshi

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,15 +1,24 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
-const ProfilePage = async  () => {
-    const { sessionClaims } = auth();
-    const userId = sessionClaims?.userId as string;
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
 
-    const organizedEvents = await getEventsByUser({ userId, page: 1 })
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+  const orders = await getOrdersByUser({ userId, page: ordersPage})
+
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
 
   return (
     <>
@@ -24,18 +33,20 @@ const ProfilePage = async  () => {
           </Button>
         </div>
       </section>
+
       <section className="wrapper my-8">
-        {/* <Collection 
+        <Collection 
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
-          emptyStateSubtext="വിഷമിക്കേണ്ട - പര്യവേക്ഷണം ചെയ്യാൻ ധാരാളം ആവേശകരമായ ഇവൻ്റുകൾ! "
+          emptyStateSubtext="No worries - plenty of exciting events to explore!"
           collectionType="My_Tickets"
           limit={3}
           page={ordersPage}
           urlParamName="ordersPage"
           totalPages={orders?.totalPages}
-        /> */}
+        />
       </section>
+
       {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
@@ -47,20 +58,19 @@ const ProfilePage = async  () => {
           </Button>
         </div>
       </section>
+
       <section className="wrapper my-8">
-         <Collection 
+        <Collection 
           data={organizedEvents?.data}
           emptyTitle="No events have been created yet"
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
           limit={3}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
           totalPages={organizedEvents?.totalPages}
-        /> 
+        />
       </section>
-      
-
     </>
   )
 }
--- next.config.js
+++ next.config.js
@@ -2,15 +2,14 @@
 const nextConfig = {
     images: {
         domains: ['utfs.io'],
-        remotePatterns : [
+        remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'utfi.io',
-                port:''
+                port: ''
             }
         ]
     }
 }
 
-
-module.exports = nextConfig
+module.exports = nextConfig;
