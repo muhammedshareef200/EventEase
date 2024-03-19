30d3d9d0ea488fbc22cb97d72ea54586dbc95011
muhammedshareef200 <128568615+muhammedshareef200@users.noreply.github.com>
2024-03-19 06:40:41 +0530

    Update validator.ts

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,21 +1,14 @@
-import { z } from "zod"
+import * as z from "zod"
 
-export const eventformSchema = z.object({
-    title: z.string().min(3, 'Title must be atleast 3 characters'),
-    description: z.string().min(3, 'Description must be atleast 3 characters').max
-    (400, 'Description must be atleast 400 characters'),
-    location: z.string().min(3, 'Location must be atleast 3 characters').max
-    (400, 'Location must be atleast 400 characters'),
-    imageUrl: z.string(),
-    startDateTime:z.date(),
-    endDateTime:z.date(),
-    categoryId:z.string(),
-    price: z.string().refine((value) => /^\d+$/.test(value), {
-      message: 'Price must contain only numbers',
-      path: ['price'],
-    }),
-  
-    isFree:z.boolean(),
-    url:z.string().url()
-  })
-  
+export const eventFormSchema = z.object({
+  title: z.string().min(3, 'Title must be at least 3 characters'),
+  description: z.string().min(3, 'Description must be at least 3 characters').max(400, 'Description must be less than 400 characters'),
+  location: z.string().min(3, 'Location must be at least 3 characters').max(400, 'Location must be less than 400 characters'),
+  imageUrl: z.string(),
+  startDateTime: z.date(),
+  endDateTime: z.date(),
+  categoryId: z.string(),
+  price: z.string(),
+  isFree: z.boolean(),
+  url: z.string().url()
+})
